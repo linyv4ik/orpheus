07d1c688d158d461ee0fc2d3024f39bf7107a4af
linyv4ik <134552208+linyv4ik@users.noreply.github.com>
2025-06-08 20:44:50 +0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -2,8 +2,6 @@
 chcp 65001 > nul
 python.exe -m pip install --upgrade pip
 pip install -r %cd%\orpheus\requirements.txt
-if not exist "%cd%\config" (
-    orpheus.py settings refresh
-)
+if not exist "%cd%\config" orpheus.py settings refresh
 echo Інсталяція завершена
 pause
