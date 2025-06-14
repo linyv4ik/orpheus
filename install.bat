0b182cb7b73c9c28e9958239bc7f7583b11fa559
linyv4ik <134552208+linyv4ik@users.noreply.github.com>
2025-06-15 00:01:22 +0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,7 +1,15 @@
 @echo off
 chcp 65001 > nul
 python.exe -m pip install --upgrade pip
-pip install -r %cd%\orpheus\requirements.txt
+pip install defusedxml
+pip install protobuf
+pip install pycryptodomex
+pip install requests
+pip install Pillow
+pip install tqdm
+pip install mutagen
+pip install ffmpeg-python
+pip install m3u8
 if not exist "%cd%\config" orpheus.py settings refresh
 echo Інсталяція завершена
 pause
